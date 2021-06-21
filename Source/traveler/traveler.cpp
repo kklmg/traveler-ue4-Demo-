4584022ba1137be405ec5c7c24e2de99b316a70f
kklmg <lm1522820366@gmail.com>
2021-06-21 14:39:45 +0900

    ue4 first commit

---
+++ Source/traveler/traveler.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "traveler.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, traveler, "traveler" );
---
+++ Source/traveler/travelerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "travelerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TRAVELER_API AtravelerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
